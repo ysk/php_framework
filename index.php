7793052cd3c56ec82a74dccbb0f30cd3a06f53d8
Yusuke Ikeda <you@example.com>
2019-11-28 22:29:43 +0900

    修正

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<?php 
+<?php
 
-
-class action {
-  public function run(){
-    $get = filter_input( INPUT_GET, 'url' );
-    $filename = './models/'.$get.'.php';
-
-        if(isset($get) && file_exists($filename)){
-            $params = explode('/', $get);
-            $model = array_shift($params);
-            require ('./models/'.$model.'.php');
-            $ret = handle($params);
-            extract($ret); //viewに変数をアサイン
-            require ('./views/'.$model.'.php');
-        } else {
-            require ('./models/index.php');
-            $ret = handle();
-            extract($ret); //viewに変数をアサイン
-            require ('./views/index.php');
-        }
-    }
-}
+require __DIR__ . '/action.php';
 
 $app = new action();
 $app->run();
-
